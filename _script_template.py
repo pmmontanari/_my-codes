04dbabb5406f3a4f2cd09198d51784a34baa2207
Pedro Montanari <pedromontanari@vivaldi.net>
2025-12-05 13:07:51 +0100

    Create _script_template.py

---
+++ _script_template.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*- (CHANGE IF NECESSARY)
+"""
+Short descriptive title.
+
+Purpose: (What this script does.)
+     Small explanation pharagraph.
+
+Context: (Why it does what it does)
+    Small explanation pharagraph.
+
+Usage: (How to use it)
+    General instructions. Limit to one phrase or two. 
+
+Created on DDD MMM DD HH:mm:SS YYYY
+
+@author: pmmontanari (pmmontanari@proton.me)
+"""
